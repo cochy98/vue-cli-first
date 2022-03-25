f985e7badb86b58092693776ef0dd5e1afc13ade
cochy98 <cosmoferrigno@gmail.com>
2022-03-25 17:09:29 +0100

    Add 'title' component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <titleComponent msg="La mia prima app con Vue CLI!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import titleComponent from './components/title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    titleComponent
   }
 }
 </script>
@@ -19,10 +18,5 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
