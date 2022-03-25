1cd9ffbdc5cb4fbde3c87bf5125f6e3142d733bb
cochy98 <cosmoferrigno@gmail.com>
2022-03-25 17:11:50 +0100

    add 'title' component

---
+++ src/components/title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="title">
+    <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'titleComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+div.title > h2{
+  text-align: center;
+  padding: 2rem;
+}
+</style>
